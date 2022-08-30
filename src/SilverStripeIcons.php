eff525e0eeef52c2b8335b640a55ea919957b8f0
CSoellinger <christopher.soellinger@gmail.com>
2022-08-30 17:27:05 +0200

    fix: Add back SilverStripeIcon class for backward compatibility

---
+++ src/SilverStripeIcons.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace LeKoala\CmsActions;
+
+use Csoellinger\Silverstripe\AdminIcons\AdminIcon;
+
+/**
+ * All icons in SilverStripe are prefixed with font-icon-{something}
+ *
+ * This class list constants that can be used as suffix
+ *
+ * Full reference here:
+ * vendor\silverstripe\admin\client\src\font\icons-reference.html
+ */
+class SilverStripeIcons
+{
+    const ICON_SEARCH = AdminIcon::SEARCH;
+    const ICON_SYNC = AdminIcon::SYNC;
+    const ICON_PRINT = AdminIcon::PRINT;
+    const ICON_LIST = AdminIcon::LIST;
+    const ICON_PLUS_CIRCLED = AdminIcon::PLUS_CIRCLED;
+    const ICON_CHECK_MARK_ = AdminIcon::CHECK_MARK;
+    const ICON_BOOK = AdminIcon::BOOK;
+    const ICON_BOOK_OPEN = AdminIcon::BOOK_OPEN;
+    const ICON_PLUS = AdminIcon::PLUS;
+    const ICON_TREE = AdminIcon::TREE;
+    const ICON_FLOW_TREE = AdminIcon::FLOW_TREE;
+    const ICON_INFO_CIRCLED = AdminIcon::INFO_CIRCLED;
+    const ICON_CHART_LINE = AdminIcon::CHART_LINE;
+    const ICON_GRAPH_BAR = AdminIcon::GRAPH_BAR;
+    const ICON_TORSOS_ALL = AdminIcon::TORSOS_ALL;
+    const ICON_TORSO = AdminIcon::TORSO;
+    const ICON_CHART_PIE = AdminIcon::CHART_PIE;
+    const ICON_SITEMAP = AdminIcon::SITEMAP;
+    const ICON_GLOBE_ = AdminIcon::GLOBE;
+    const ICON_CHAT = AdminIcon::CHAT;
+    const ICON_COMMENT = AdminIcon::COMMENT;
+    const ICON_LOGOUT = AdminIcon::LOGOUT;
+    const ICON_CANCEL_CIRCLED = AdminIcon::CANCEL_CIRCLED;
+    const ICON_LEFT_OPEN = AdminIcon::LEFT_OPEN;
+    const ICON_RIGHT_OPEN = AdminIcon::RIGHT_OPEN;
+    const ICON_CHECK_MARK = AdminIcon::CHECK_MARK;
+    const ICON_CHECK_MARK_CIRCLE = AdminIcon::CHECK_MARK_CIRCLE;
+    const ICON_BACK_IN_TIME = AdminIcon::BACK_IN_TIME;
+    const ICON_COG = AdminIcon::COG;
+    const ICON_FAST_FORWARD = AdminIcon::FAST_FORWARD;
+    const ICON_INSTALL = AdminIcon::INSTALL;
+    const ICON_DOWN_CIRCLED = AdminIcon::DOWN_CIRCLED;
+    const ICON_EYE = AdminIcon::EYE;
+    const ICON_COLUMNS = AdminIcon::COLUMNS;
+    const ICON_EDIT_WRITE = AdminIcon::EDIT_WRITE;
+    const ICON_MONITOR = AdminIcon::MONITOR;
+    const ICON_MOBILE = AdminIcon::MOBILE;
+    const ICON_TABLET = AdminIcon::TABLET;
+    const ICON_PENCIL = AdminIcon::PENCIL;
+    const ICON_TRASH = AdminIcon::TRASH;
+    const ICON_EDIT = AdminIcon::EDIT;
+    const ICON_TICK = AdminIcon::TICK;
+    const ICON_UPLOAD = AdminIcon::UPLOAD;
+    const ICON_THUMBNAILS = AdminIcon::THUMBNAILS;
+    const ICON_DOWN_OPEN = AdminIcon::DOWN_OPEN;
+    const ICON_UP_OPEN = AdminIcon::UP_OPEN;
+    const ICON_LINK = AdminIcon::LINK;
+    const ICON_LEVEL_UP = AdminIcon::LEVEL_UP;
+    const ICON_IMAGE = AdminIcon::IMAGE;
+    const ICON_TRASH_BIN = AdminIcon::TRASH_BIN;
+    const ICON_CODE = AdminIcon::CODE;
+    const ICON_LEFT_OPEN_BIG = AdminIcon::LEFT_OPEN_BIG;
+    const ICON_DOWN_OPEN_BIG = AdminIcon::DOWN_OPEN_BIG;
+    const ICON_UP_OPEN_BIG = AdminIcon::UP_OPEN_BIG;
+    const ICON_RIGHT_OPEN_BIG = AdminIcon::RIGHT_OPEN_BIG;
+    const ICON_DOT_ = AdminIcon::DOT_3;
+    const ICON_SAVE = AdminIcon::SAVE;
+    const ICON_ROCKET = AdminIcon::ROCKET;
+    const ICON_CANCEL = AdminIcon::CANCEL;
+    const ICON_HAPPY = AdminIcon::HAPPY;
+    const ICON_SAD = AdminIcon::SAD;
+    const ICON_ANGLE_DOUBLE_RIGHT = AdminIcon::ANGLE_DOUBLE_RIGHT;
+    const ICON_ANGLE_DOUBLE_LEFT = AdminIcon::ANGLE_DOUBLE_LEFT;
+    const ICON_ANGLE_RIGHT = AdminIcon::ANGLE_RIGHT;
+    const ICON_ANGLE_LEFT = AdminIcon::ANGLE_LEFT;
+    const ICON_CARET_UP_DOWN = AdminIcon::CARET_UP_DOWN;
+    const ICON_CARET_DOWN_TWO = AdminIcon::CARET_DOWN_TWO;
+    const ICON_CARET_UP_TWO = AdminIcon::CARET_UP_TWO;
+    const ICON_RIGHT_DIR = AdminIcon::RIGHT_DIR;
+    const ICON_DOWN_DIR = AdminIcon::DOWN_DIR;
+    const ICON_LINK_BROKEN = AdminIcon::LINK_BROKEN;
+    const ICON_SWITCH = AdminIcon::SWITCH;
+    const ICON_RESIZE = AdminIcon::RESIZE;
+    const ICON_MENU = AdminIcon::MENU;
+    const ICON_EDIT_LIST = AdminIcon::EDIT_LIST;
+    const ICON_HELP_CIRCLED = AdminIcon::HELP_CIRCLED;
+    const ICON_ICON_ENLARGE = AdminIcon::ICON_ENLARGE;
+    const ICON_PAGE_MULTIPLE = AdminIcon::PAGE_MULTIPLE;
+    const ICON_DATABASE = AdminIcon::DATABASE;
+    const ICON_EXTERNAL_LINK = AdminIcon::EXTERNAL_LINK;
+    const ICON_ATTENTION = AdminIcon::ATTENTION;
+    const ICON_BLOCK = AdminIcon::BLOCK;
+    const ICON_UP_CIRCLED = AdminIcon::UP_CIRCLED;
+    const ICON_CLOCK = AdminIcon::CLOCK;
+    const ICON_SPINNER = AdminIcon::SPINNER;
+    const ICON_MENU_CAMPAIGNS = AdminIcon::MENU_CAMPAIGNS;
+    const ICON_MENU_FILES = AdminIcon::MENU_FILES;
+    const ICON_MENU_HELP = AdminIcon::MENU_HELP;
+    const ICON_MENU_MODALADMIN = AdminIcon::MENU_MODALADMIN;
+    const ICON_MENU_PAGES = AdminIcon::MENU_PAGES;
+    const ICON_MENU_REPORTS = AdminIcon::MENU_REPORTS;
+    const ICON_MENU_SECURITY = AdminIcon::MENU_SECURITY;
+    const ICON_MENU_SETTINGS = AdminIcon::MENU_SETTINGS;
+    const ICON_SILVERSTRIPE = AdminIcon::SILVERSTRIPE;
+    const ICON_SORT = AdminIcon::SORT;
+    const ICON_LOCK = AdminIcon::LOCK;
+    const ICON_CROSS_MARK = AdminIcon::CROSS_MARK;
+    const ICON_FOLDER_ADD = AdminIcon::FOLDER_ADD;
+    const ICON_DRAG_HANDLE = AdminIcon::DRAG_HANDLE;
+    const ICON_SAFARI = AdminIcon::SAFARI;
+    const ICON_FOLDER_MOVE = AdminIcon::FOLDER_MOVE;
+    const ICON_CLIPBOARD_PENCIL = AdminIcon::CLIPBOARD_PENCIL;
+    const ICON_PICTURE = AdminIcon::PICTURE;
+    const ICON_SLIDERS = AdminIcon::SLIDERS;
+    const ICON_WHITE_QUESTION = AdminIcon::WHITE_QUESTION;
+    const ICON_HOME = AdminIcon::HOME;
+    const ICON_BLOCK_BACK = AdminIcon::BLOCK_BACK;
+    const ICON_BLOCK_BANNER = AdminIcon::BLOCK_BANNER;
+    const ICON_BLOCK_CAROUSEL = AdminIcon::BLOCK_CAROUSEL;
+    const ICON_BLOCK_CONTENT = AdminIcon::BLOCK_CONTENT;
+    const ICON_BLOCK_FILE = AdminIcon::BLOCK_FILE;
+    const ICON_BLOCK_FILE_LIST = AdminIcon::BLOCK_FILE_LIST;
+    const ICON_BLOCK_FORM = AdminIcon::BLOCK_FORM;
+    const ICON_BLOCK_LAYOUT = AdminIcon::BLOCK_LAYOUT;
+    const ICON_BLOCK_MEDIA = AdminIcon::BLOCK_MEDIA;
+    const ICON_TAG = AdminIcon::TAG;
+    const ICON_TAGS = AdminIcon::TAGS;
+    const ICON_CIRCLE_STAR = AdminIcon::CIRCLE_STAR;
+    const ICON_CHECKLIST = AdminIcon::CHECKLIST;
+    const ICON_EXPORT = AdminIcon::EXPORT;
+    const ICON_BOX = AdminIcon::BOX;
+    const ICON_LANGUAGE = AdminIcon::LANGUAGE;
+    const ICON_GLOBE = AdminIcon::GLOBE;
+    const ICON_EYE_WITH_LINE = AdminIcon::EYE_WITH_LINE;
+    const ICON_TRANSLATABLE = AdminIcon::TRANSLATABLE;
+}
